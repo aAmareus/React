f9fc2bc73d22e8a104e3219b25e679ebf2304fa6
aAmareus <gonzalezamareus@gmail.com>
2025-09-23 22:23:13 -0300

    1.1.5 - Instalacion de dependencia de Iconos FontAwesome. Creacion y estilización del trigger para mostrar/ocultar contraseña. Pendiente lo del commit anterior. [Commit: 1.1.4]

--- my-app-v2/src/components/pageComponents/forms/LoginForm.jsx
+++ my-app-v2/src/components/pageComponents/forms/LoginForm.jsx
@@ -4,10 +4,22 @@ import { Link, Navigate, useNavigate } from 'react-router-dom';
 // Importing Custom CSS
 import '../../assets/css/LoginForm.css'
 
+// Importing Bootstrap components
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
 
-import LogoText from '../../assets/img/logoBN.png'
+// Importing FontAwesome Icons
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+
+library.add(fas, far, fab);
+
+// Importing Images
+import LogoText from '../../assets/img/logoBN.png';
 
 
 function LoginForm() {
@@ -15,6 +27,7 @@ function LoginForm() {
     // Function to validate form
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [showPassword, setShowPassword] = useState(false); // To show n' hide password
     const navigate = useNavigate();
 
     const handleSubmit = () => {
@@ -26,8 +39,6 @@ function LoginForm() {
             alert('Credenciales inválidas. Intente nuevamente.');
         }
     }
-
-
 
     return (
         <>  {/* Main Container */}
@@ -58,12 +69,24 @@ function LoginForm() {
                         className='mb-3'
                     >
                         <Form.Control 
-                        type='password' 
+                        type={showPassword ? 'text' : 'password'} 
                         placeholder='Contraseña' 
                         className='input' 
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                         />
+
+                        {/* Trigger to see n' hide password */}
+
+                        <div className="trigger">
+                            <input type="checkbox" 
+                            className='toggle-check'
+                            checked={showPassword}
+                            onChange={() => setShowPassword(!showPassword)} // Cambia al hacer click
+                            />
+                            <FontAwesomeIcon icon={showPassword ? 'fa-eye' : 'fa-eye-slash'} 
+                            className='toggle-icon'/>
+                        </div>
                     </FloatingLabel>
 
                     <div className="link-iitems d-flex flex-column align-items-center justify-content-center mt-3">
